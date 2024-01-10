b2f9d67106ba4f6821f457a71a5212af86d2fcef
EL-MANANIFatima <fatimaelmanani80@gmail.com>
2024-01-10 19:48:34 +0100

    adding the deleted app code

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -8,6 +8,200 @@ function Applications() {
         <>
             <Header />
             <Breadcrumb/>
+            <section className="w3l-contact-1 pb-5" id="contact" style={{ display: "flex", justifyContent: "center" }}>
+            <div id="main-content">
+                <article id="post-3786" className="post-3786 page type-page status-publish hentry">
+                    <div className="entry-content">
+                        <div className="at-above-post-page addthis_tool" data-url="https://www.ds3-datascience-polytechnique.fr/key-dates-application/"></div>
+                        <div id="et-boc" className="et-boc">
+                            <div className="et-l et-l--post">
+                                <div className="et_builder_inner_content et_pb_gutters3">
+                                    <div className="et_pb_section et_pb_section_0 et_pb_with_background et_section_regular">
+                                            <div class="et_pb_row et_pb_row_0">
+                                                <div className="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
+        
+                                                    <div
+                                                        className="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_center et_pb_bg_layout_dark">
+            
+            
+                                                        <div className="et_pb_text_inner">
+                                                            <h1>FEES &amp; APPLICATION</h1>
+                                                        </div>
+                                                    </div>
+                                                    <div
+                                                        className="et_pb_module et_pb_divider et_pb_divider_0 et_pb_divider_position_ et_pb_space">
+                                                        <div className="et_pb_divider_internal"></div>
+                                                    </div>
+                                                </div> 
+            
+            
+                                            </div> 
+                                            <div className="et_pb_row et_pb_row_1 et_pb_equal_columns">
+                                                <div
+                                                    className="et_pb_column et_pb_column_1_3 et_pb_column_1  et_pb_css_mix_blend_mode_passthrough">
+            
+            
+                                                    <div
+                                                        className="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_dark">
+            
+            
+                                                        <div className="et_pb_text_inner">
+                                                            <h4 style={{textAlign: "center"}}><strong><span
+                                                                        style={{color: "#fe79a2"}}>PLENARY TALKS</span></strong></h4>
+                                                            <p style={{textAlign: "center"}}><span style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#3f3a64"}}><strong>Students* (MSc2, PhD),
+                                                                            PostDocs:</strong></span>&nbsp;</span><strong><span
+                                                                        style={{fontSize: "medium"}}>50€</span></strong></p>
+                                                            <p style={{textAlign: "center"}}><span
+                                                                    style={{textAlign: "left", fontSize: "16px"}}><span
+                                                                        style={{color: "#3f3a64"}}><b>Academics &amp; Members of public
+                                                                            institutions</b></span></span><span
+                                                                    style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#cd5c5c"}}><strong>:</strong></span>&nbsp;<strong>100€</strong></span>
+                                                            </p>
+                                                            <p style={{textAlign: "center"}}><span style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#3f3a64"}}><strong>Professionals:</strong>
+                                                                    </span><strong> 100€</strong></span></p>
+                                                        </div>
+                                                    </div>
+                                                </div> 
+                                                <div
+                                                    className="et_pb_column et_pb_column_1_3 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough">
+            
+            
+                                                    <div
+                                                        className="et_pb_module et_pb_text et_pb_text_2  et_pb_text_align_left et_pb_bg_layout_dark">
+            
+            
+                                                        <div className="et_pb_text_inner">
+                                                            <h4 style={{textAlign: "center"}}><strong><span
+                                                                        style={{color: "#fe79a2"}}>TUTORIALS</span></strong></h4>
+                                                            <p style={{textAlign: "center"}}>(the prices shown are per tutorial
+                                                                session)<strong><span style={{color: "#ffffff"}}></span></strong></p>
+                                                            <p style={{textAlign:"center"}}><span style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#3f3a64"}}><strong>Students* (MSc2, PhD),
+                                                                            PostDocs:</strong></span>&nbsp;</span><strong><span
+                                                                        style={{fontSize: "medium"}}>150€</span></strong></p>
+                                                            <p style={{textAlign: "center"}}><span
+                                                                    style={{textAlign: "left", fontSize: "16px"}}><span
+                                                                        style={{color: "#3f3a64"}}><b>Academics &amp; Members of public
+                                                                            institutions</b></span></span><span
+                                                                    style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#cd5c5c"}}><strong>:</strong></span>&nbsp;<strong>200€</strong></span>
+                                                            </p>
+                                                            <p style={{textAlign: "center"}}><span style={{fontSize: "medium"}}><span
+                                                                        style={{color: "#3f3a64"}}><strong>Professionals:</strong>
+                                                                    </span><strong> 200€</strong></span></p>
+                                                        </div>
+                                                    </div> 
+                                                </div> 
+                                                <div
+                                                    className="et_pb_column et_pb_column_1_3 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child">
+            
+            
+                                                    <div
+                                                        className="et_pb_module et_pb_text et_pb_text_3  et_pb_text_align_left et_pb_bg_layout_dark">
+            
+            
+                                                        <div className="et_pb_text_inner">
+                                                            <h4 style={{textAlign: "center"}}>Application deadline:</h4>
+                                                            <p style={{textAlign: "center"}}><span style={{color: "#3f3a64"}}><b>October
+                                                                        21, 2024</b></span></p>
+                                                            <h4 style={{textAlign: "center"}}><span className="s1">Notification of
+                                                                    acceptance&nbsp;by</span></h4>
+                                                            <p style={{textAlign: "center"}}><span color="#cd5c5c"
+                                                                    style={{color: "#cd5c5c"}}><span
+                                                                        style={{color: "#3f3a64"}}><b>November 4,
+                                                                            2024</b></span></span></p>
+                                                        </div>
+                                                    </div>
+                                                </div> 
+            
+            
+                                            </div> 
+                                            <div className="et_pb_row et_pb_row_2">
+                                                <div
+                                                    className="et_pb_column et_pb_column_4_4 et_pb_column_4  et_pb_css_mix_blend_mode_passthrough et-last-child">
+            
+            
+                                                    <div
+                                                        className="et_pb_module et_pb_divider et_pb_divider_1 et_pb_divider_position_ et_pb_space">
+                                                        <div className="et_pb_divider_internal"></div>
+                                                    </div>
+                                                    <div
+                                                        className="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_button_alignment_center et_pb_module ">
+                                                        <Link
+                                                            className="et_pb_button et_pb_custom_button_icon et_pb_button_0 centertex et_pb_bg_layout_light"
+                                                            to="/form"
+                                                            data-icon=""
+                                                        >
+                                                        ADD APPLICATION
+                                                        </Link>
+
+                                                    </div>
+                                                    <div
+                                                        className="et_pb_module et_pb_text et_pb_text_4  et_pb_text_align_center et_pb_bg_layout_dark">
+            
+            
+                                                        <div className="et_pb_text_inner">
+                                                            <h1 style={{fontWeight: "500", textAlign: "center"}}><strong>Important
+                                                                    guidelines to the application:</strong></h1>
+                                                            <ol style={{textAlign: "center" ,listStyleType: "decimal" }}>
+                                                                <li style={{fontWeight: "500"}}>The deadlines are meant in Pacific
+                                                                    Time (5 PM). Click on the link above that will redirect you to
+                                                                    the application form.</li>
+                                                            </ol>
+                                                            <strong><p style={{fontWeight: "500", textAlign: "center"}}>On the form:</p></strong>
+                                                            <p style={{fontWeight: "500", textAlign: "center"}}>– Please indicate your
+                                                                motivation for applying to the summer school&nbsp;(one sentence) in
+                                                                the “Title” section.</p>
+                                                            <p style={{fontWeight: "500", textAlign: "center"}}>– Upload your CV in the
+                                                                “Files” section (mandatory). Make sure that your uploaded file (.pdf) is in
+                                                                English.</p>
+                                                            <p style={{fontWeight: "500", textAlign: "center"}}>– In this section you
+                                                                will be able to propose a poster.</p>
+                                                            <p style={{fontWeight: "500", textAlign: "center"}}>– Click on “Submit” to
+                                                                send your application; you can edit your&nbsp;application until
+                                                                October 21, 2024.</p>
+                                                            <p style={{fontWeight: "500", textAlign: "center"}}>– Please do not submit
+                                                                multiple applications <img draggable="false" className="emoji" alt="😉"
+                                                                    src="https://s.w.org/images/core/emoji/11/svg/1f609.svg"/></p>
+                                                            <ol start="2">
+                                                                <li style={{fontWeight: "500", textAlign: "center"}}>Your application will then be reviewed by the organizers. You will receive a notification in email on November 4, 2024. If you do not get it, please check your spam folder.</li>
+                                                                <li style={{fontWeight: "500", textAlign: "center"}}>Upon acceptance, the email will include a personal link and ID to confirm your registration and proceed with the payment.</li>
+                                                                <li style={{fontWeight: "500", textAlign: "center"}}>In case of cancellation of registration by a participant, the registration fee will be refunded as follows:
+                                                                    <ul>
+                                                                        <li>cancellation before December 1st, 2024: full refund</li>
+                                                                        <li>cancellation between December 1 and December 14, 2024: partial refund of 50% of the amount paid</li>
+                                                                        <li>cancellation after December 14, 2024: no refund</li>
+                                                                    </ul>
+                                                                </li>
+                                                            </ol>
+
+                                                            <p><em><strong><span className="s1">*</span></strong><span
+                                                                        style={{fontSize: "12px", fontFamily: "Helvetica", WebkitTextStroke: "#000000"}}>MSc2:
+                                                                        last year master students, graduating by August
+                                                                        2023.</span></em></p>
+                                                        </div>
+                                                    </div>
+                                                </div> 
+            
+            
+                                            </div> 
+            
+            
+                                        </div> 
+                                    </div>
+                                </div>
+            
+            
+                            </div>
+                            <div className="at-below-post-page addthis_tool"
+                                data-url="https://www.ds3-datascience-polytechnique.fr/key-dates-application/"></div>
+                        </div> 
+                </article>
+            </div>   
+            </section>
             <Footer />
         </>
     )
